59b4bf8e1e4f8482f6c6ffd237a895552e35abd9
AJ <adhilmp3@gmail.com>
2020-02-23 22:56:16 +0530

    fin

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, Image, StyleSheet, Text} from 'react-native';
 import Dialogflow from 'react-native-dialogflow';
 import Tts from 'react-native-tts';
 
@@ -21,7 +21,16 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-    Tts.speak('Welcome to Vis-Aid. What do you want?');
+    Tts.speak(
+      'Welcome to Vis-Aid , An app to help visually challanged people and kids to understand the object infront of them and also to read any text using our text reader.',
+    );
+    Tts.speak('How to use');
+    Tts.speak(
+      'To open object recognition, Press the mic and speak OPEN CAMERA.',
+    );
+    Tts.speak(
+      'To open text recognition, Press the mic and speak OPEN TEXT READER.',
+    );
   }
 
   gotSpeech = data => {
@@ -49,10 +58,24 @@ export default class Home extends React.Component {
             );
           }}>
           <Image
-            source={require('../assets/brand.png')}
-            style={{width: 100, height: 100}}
+            source={require('../assets/voice-control.png')}
+            style={styles.image}
           />
         </TouchableOpacity>
+        <View style={styles.card}>
+          <Text style={styles.content}>
+            Welcome to Vis-Aid , An app to help visually challanged people and
+            kids to understand the object infront of them and also to read any
+            text using our text reader.
+          </Text>
+          <Text style={styles.title}>How to use :</Text>
+          <Text style={styles.content}>
+            To open object recognition, Press the mic and speak OPEN CAMERA.
+          </Text>
+          <Text style={styles.content}>
+            To open text recognition, Press the mic and speak OPEN TEXT READER.
+          </Text>
+        </View>
       </View>
     );
   }
@@ -61,8 +84,38 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingVertical: 50,
+  },
+  card: {
+    width: '80%',
+    height: '40%',
+    backgroundColor: 'transparent',
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 3,
+    marginVertical: 40,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginVertical: 40,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Open Sans',
+    marginVertical: 10,
+  },
+  content: {
+    fontFamily: 'Open Sans',
+    fontSize: 15,
+    marginVertical: 5,
   },
 });
